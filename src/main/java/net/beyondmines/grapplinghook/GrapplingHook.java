e477d310dbb300174fd791161a92432ab4b6c3e2
Alexander Naber <naberalex@gmx.de>
2024-09-28 16:04:59 +0200

    License and first steps

---
+++ src/main/java/net/beyondmines/grapplinghook/GrapplingHook.java
@@ -0,0 +1,13 @@
+package net.beyondmines.grapplinghook;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GrapplingHook.MODID)
+public class GrapplingHook {
+
+	public static final String MODID = "bm_grapplinghook";
+
+	public static final Logger LOGGER = LogUtils.getLogger();
+}
